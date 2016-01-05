9a44af86624477233a069a6117b92f1dcbe57c73
ncthbrt <nick@cuthbert.co.za>
2016-01-05 19:54:16 +0200

    Moved C# SDK to different repo so as to not trigger a rebuild on every SDK change.

---
+++ C#/Wimt.OtherTree.Client/ChargeReference.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Wimt.OtherTree.References
+{
+    [Serializable]
+    public class ChargeReference : IEquatable<ChargeReference>
+    {
+        public bool Equals(ChargeReference other)
+        {
+            if (ReferenceEquals(null, other)) return false;
+            if (ReferenceEquals(this, other)) return true;
+            return Id.Equals(other.Id) && Charge.Equals(other.Charge);
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != this.GetType()) return false;
+            return Equals((ChargeReference)obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                return (Id.GetHashCode() * 397) ^ Charge.GetHashCode();
+            }
+        }
+
+
+
+        public Guid Charge
+        {
+            get { return _charge; }
+            set { _charge = value; }
+        }
+
+        [DataMember]
+        public Guid _charge = Guid.Empty;
+        [DataMember]
+        public Guid _id = Guid.Empty;
+
+        public Guid Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public ChargeReference(Guid charge)
+        {
+            Charge = charge;
+            Id = Guid.NewGuid();
+        }
+
+        public ChargeReference(Guid charge, Guid id)
+        {
+            Charge = charge;
+            Id = id;
+        }
+
+        public ChargeReference()
+        {
+
+            Charge = Guid.Empty;
+            Id = Guid.Empty;
+        }
+
+
+    }
+}
---
+++ C#/Wimt.OtherTree.Client/OtherTreeClient.cs
@@ -0,0 +1,190 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Google.Protobuf;
+using System.Runtime;
+using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNet.SignalR.Client.Hubs;
+using Microsoft.AspNet.SignalR.Client.Transports;
+using Wimt.OtherTree.References;
+using Version = Wimt.OtherTree.Version;
+
+namespace OtherTreeClientSDK
+{
+    // This project can output the Class library as a NuGet Package.
+    // To enable this option, right-click on the project and select the Properties menu item. In the Build tab select "Produce outputs on build".
+    public delegate void OnThud<T>(T message) where T: IMessage<T>, new();
+
+    public class TypeDescription
+    {
+        public string TypeName { get; set; }
+        public Version Version { get; set; }
+
+        public Type Type { get; set; }
+        protected TypeDescription(Type type, string typeName, Version? version)
+        {
+            Type = type;
+            TypeName = typeName;
+            Version = version.HasValue?version.Value: new Version();
+        }
+    }
+
+    public class TypeDescription<TMessage>: TypeDescription
+        where TMessage : IMessage<TMessage>, new ()
+    {
+        public TypeDescription(Version? version):base(typeof(TMessage),typeof(TMessage).FullName,version)
+        {
+            
+        }
+
+        public TypeDescription() : base(typeof (TMessage), typeof (TMessage).FullName, null)
+        {
+        }
+    }
+
+    public class OtherTreeClient: IDisposable
+    {
+        private string _url;
+        private bool _connected = false;
+        private string _token;
+        private IList<KeyValuePair<TypeDescription, Delegate>> _handlers;
+        private HubConnection _hubConnection;
+        private IHubProxy _bedrockProxy;
+        public OtherTreeClient(string url, string token,bool useDefaultUrl=true)
+        {
+            _url = url;
+            _token = token;
+            var qs = new Dictionary<string, string>();
+            qs.Add("username", token);
+            _handlers=new List<KeyValuePair<TypeDescription, Delegate>>();            
+            _hubConnection = new HubConnection(_url,token,useDefaultUrl);
+
+            //  TODO Add proper auth        
+            //  var cache= new CredentialCache();
+            //  cache.Add(/*Add credentials here*/);
+            //  hubConnection.Credentials = cache;
+            
+            _bedrockProxy= _hubConnection.CreateHubProxy("Bedrock");          
+        }
+
+        public void Connect()
+        {
+            ConnectAsync().Wait();
+        }
+
+        public async Task ConnectAsync()
+        {            
+            await _hubConnection.Start(new WebSocketTransport());
+
+            _bedrockProxy.On("Thud", (string typeName, Wimt.OtherTree.Version? version, byte[] payload) =>
+            {
+                foreach (var handler in _handlers)
+                {
+                    if (typeName == handler.Key.TypeName &&
+                        (!version.HasValue || handler.Key.Version.IsCompatableWith(version.Value)))
+                    {
+                        var sap = (Google.Protobuf.IMessage)(handler.Key.Type.GetConstructor(new Type[] { }).Invoke(new object[] { }));
+                        sap.MergeFrom(payload);
+                        handler.Value.DynamicInvoke(new object[] { sap });
+                    }
+                }
+            });
+
+            _connected = true;
+        }
+
+        public void AddThudHandler<T>(OnThud<T> handler, Version? version = null)
+            where T : IMessage<T>,new()
+        {
+            if (version.HasValue)
+            {
+                _handlers.Add(new KeyValuePair<TypeDescription, Delegate>(new TypeDescription<T>(version),handler));
+            }
+            else
+            {
+                _handlers.Add(new KeyValuePair<TypeDescription, Delegate>(new TypeDescription<T>(), handler));
+            }            
+        }
+
+        public void RemoveThudHandler<T>(OnThud<T> handler)
+            where T : IMessage<T>, new()
+        {
+            for (int i = _handlers.Count-1; i >=0; --i)
+            {
+                if (_handlers[i].Value.GetInvocationList().Contains(handler))
+                {
+                    _handlers.RemoveAt(i);
+                }
+            }
+        }
+
+        public ChargeReference Charge<T>(IMessage<T> charge, Version? chargeVersion = null)
+            where T : IMessage<T>, new()            
+        {
+            var chargeTask = ChargeAsync(charge, chargeVersion);
+            chargeTask.Wait();            
+            return chargeTask.Result;
+        }
+
+
+        public async Task<ChargeReference> ChargeAsync<T>(IMessage<T> charge, Version? chargeVersion = null)
+            where T : IMessage<T>, new()            
+        {
+            if (!_connected)
+            {
+                await ConnectAsync();
+            }
+            
+            return await _bedrockProxy.Invoke<ChargeReference>("Charge",typeof (T).FullName,chargeVersion.HasValue ? chargeVersion.Value : new Version(), charge.ToByteArray());
+        }
+
+        public void Discharge(ChargeReference reference)
+        {
+            DischargeAsync(reference).Wait();
+        }
+
+        public async  Task DischargeAsync(ChargeReference reference)
+        {
+            if (!_connected)
+            {
+                await ConnectAsync();
+            }            
+            await _bedrockProxy.Invoke("Discharge",reference);
+        }
+
+
+        public void Water<T>(T water, Version? version = null) where T : IMessage<T>, new()
+        {
+            WaterAsync<T>(water, version).Wait();
+        }
+
+        public async Task WaterAsync<T>(T water, Version? version = null) where T : IMessage<T>, new()
+        {
+            if (!version.HasValue)
+            {
+                version=new Version();
+            }
+        
+            await _bedrockProxy.Invoke("Water",typeof(T).FullName, version,water.ToByteArray());            
+        }
+
+        public void Disconnect()
+        {
+            if (_connected)
+            {
+                _hubConnection.Stop();
+                _connected = false;
+            }
+        }
+        public void Dispose()
+        {
+            if (_connected)
+            {
+                _hubConnection.Dispose();
+                _connected = false;
+            }
+            
+        }
+    }
+}
