48fc781af80672e19836a77979c8cbdd41a6bd0e
ncthbrt <nick@cuthbert.co.za>
2016-01-06 16:22:29 +0200

    Java prototype usage of SDK is working

--- Java/client/src/wimt/othertree/client/AnnotatedOnThudListener.java
+++ Java/client/src/wimt/othertree/client/AnnotatedOnThudListener.java
@@ -1,33 +1,44 @@
 package wimt.othertree.client;
 
-import com.google.protobuf.GeneratedMessage;
+import com.google.protobuf.Descriptors;
 import com.google.protobuf.InvalidProtocolBufferException;
+import com.google.protobuf.Message;
+import java.lang.reflect.InvocationTargetException;
+
 
 /**
  * Created by Nick Cuthbert on 05/01/2016.
  */
-class AnnotatedOnThudListener<T extends GeneratedMessage> {
+class AnnotatedOnThudListener<T extends Message> {
     public String typeName="";
     public Version version;
     public ThudListener<T> thudListener;
-    private T.Builder builder;
+    private Class<T> expectedType;
 
-    public AnnotatedOnThudListener(ThudListener<T> thudListener){
-        this(thudListener,new Version());
+
+    public AnnotatedOnThudListener(Class<T> expectedType,ThudListener<T> thudListener){
+        this(expectedType, thudListener, new Version());
     }
 
-    public AnnotatedOnThudListener(ThudListener<T> thudListener, Version version){
+
+
+    public AnnotatedOnThudListener(Class<T> expectedType, ThudListener<T> thudListener, Version version){
         if(version==null){
             version=new Version();
         }
+        this.expectedType=expectedType;
+
         this.version=version;
         this.thudListener=thudListener;
+//
 
-        Class<T.Builder> builderClass = T.Builder.class;
         try {
-            builder = builderClass.newInstance();
-            typeName= this.builder.getDescriptorForType().getFullName();
-        } catch (InstantiationException | IllegalAccessException e) {
+            Descriptors.Descriptor descriptor = ((Message.Builder)(expectedType.getMethod("newBuilder").invoke(null))).getDescriptorForType();
+            this.typeName= OtherTreeClient.toPascalCase(descriptor.getFullName());
+        }
+        catch (NoSuchMethodException | InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
             e.printStackTrace();
         }
     }
@@ -37,9 +48,16 @@ class AnnotatedOnThudListener<T extends GeneratedMessage> {
     }
 
     public void onThud(byte[] payload) throws InvalidProtocolBufferException {
-        builder.mergeFrom(payload);
-        T message= (T) builder.build();
-        thudListener.onThud(message);
+        try {
+            T.Builder builder=(T.Builder)(expectedType.getMethod("newBuilder").invoke(null));
+            thudListener.onThud((T)(builder.mergeFrom(payload).build()));
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        }
     }
 }
 
--- Java/client/src/wimt/othertree/client/OtherTreeClient.java
+++ Java/client/src/wimt/othertree/client/OtherTreeClient.java
@@ -2,21 +2,28 @@ package wimt.othertree.client;
 import com.google.protobuf.GeneratedMessage;
 import com.google.protobuf.InvalidProtocolBufferException;
 
-import microsoft.aspnet.signalr.client.*;
-import microsoft.aspnet.signalr.client.hubs.SubscriptionHandler3;
-import microsoft.aspnet.signalr.client.transport.*;
-import microsoft.aspnet.signalr.client.hubs.HubConnection;
-import microsoft.aspnet.signalr.client.hubs.HubProxy;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 
+import microsoft.aspnet.signalr.client.Action;
+import microsoft.aspnet.signalr.client.Logger;
+import microsoft.aspnet.signalr.client.NullLogger;
+import microsoft.aspnet.signalr.client.SignalRFuture;
+import microsoft.aspnet.signalr.client.hubs.HubConnection;
+import microsoft.aspnet.signalr.client.hubs.HubProxy;
+import microsoft.aspnet.signalr.client.hubs.SubscriptionHandler3;
+import microsoft.aspnet.signalr.client.transport.LongPollingTransport;
+
 /**
  * Created by Nick Cuthbert on 05/01/2016.
  */
+
+
 public class OtherTreeClient implements AutoCloseable {
+
+
 
     protected String url;
     protected String token;
@@ -24,59 +31,76 @@ public class OtherTreeClient implements AutoCloseable {
     private HubProxy hub;
     private boolean connected=false;
     private List<AnnotatedOnThudListener> onThudListeners;
+    private Logger logger;
 
     public OtherTreeClient(String url, String token){
         this(url, token, true);
     }
 
     public OtherTreeClient(String url, String token,boolean useDefaultUrl){
+        this(url, token, useDefaultUrl, new NullLogger());
+    }
+
+    public OtherTreeClient(String url, String token,boolean useDefaultUrl, Logger logger){
         init(url,token,useDefaultUrl);
         onThudListeners=new ArrayList<>();
+        this.logger=logger;
     }
 
     protected OtherTreeClient(){
+        this("","",true,new NullLogger());
     }
+
+
 
     protected void  init(String url, String token,boolean useDefaultUrl){
         this.url=url;
         this.token=token;
         HashMap<Object, Object> dict = new HashMap<Object, Object>();
         String query = "&username="+ token;
-        connection= new HubConnection(url, query, useDefaultUrl, new NullLogger());
+        connection= new HubConnection(url, query, useDefaultUrl, logger);
     }
 
 
     public SignalRFuture<Void> futureConnection(){
         hub = connection.createHubProxy("Bedrock");
 
-        ArrayList<? extends Integer> integers = new ArrayList<>();
-        integers.getClass();
-        hub.on("Thud", new SubscriptionHandler3<String, Version, Integer[]>() {
+
+        hub.on("Thud", new SubscriptionHandler3<String, Version, String>() {
             @Override
-            public void run(String type, Version version, Integer[] payload) {
+            public void run(String type, Version version, String payload){
                 byte[] bytes=toByteArray(payload);
                 for (AnnotatedOnThudListener handler: onThudListeners) {
-                        if(handler.applicable(type,version)){
-                            try {
-                                handler.onThud(toByteArray(payload));
-                            } catch (InvalidProtocolBufferException e) {
-                                e.printStackTrace();
-                            }
+                    if(handler.applicable(type,version)){
+                        try {
+                            handler.onThud(bytes);
+                        } catch (InvalidProtocolBufferException e) {
+                            e.printStackTrace();
                         }
+                    }
                 }
             }
-        }, String.class, Version.class, Integer[].class);
+        }, String.class, Version.class, String.class);
 
-        SignalRFuture<Void> futureConnectionTask = connection.start(new AutomaticTransport(connection.getLogger()));
-        return futureConnectionTask.done(new Action<Void>() {
+
+        SignalRFuture<Void> futureConnectionTask = connection.start(new LongPollingTransport(connection.getLogger()));
+        futureConnectionTask = futureConnectionTask.done(new Action<Void>() {
             @Override
             public void run(Void aVoid) throws Exception {
-                synchronized (this){
-                    connected=true;
+
+                synchronized (this) {
+                    connected = true;
                 }
             }
         });
+
+        return futureConnectionTask;
     }
+
+    byte[] toByteArray(String hexString){
+        return Base64.decode(hexString, Base64.DEFAULT);
+    }
+
     public void connect() throws ExecutionException, InterruptedException {
         futureConnection().get();
     }
@@ -89,19 +113,11 @@ public class OtherTreeClient implements AutoCloseable {
         return ints;
     }
 
-    private static byte[] toByteArray(Integer[] ints){
-        final byte[] bytes= new byte[ints.length];
-        for(int i=0;i<ints.length;i++){
-            bytes[i]=(byte)((int)ints[i]);
-        }
-        return bytes;
-    }
-
 
 
     public <T extends com.google.protobuf.GeneratedMessage> SignalRFuture<Void> futureWater(T water, Version version){
         int[] ints= toIntArray(water.toByteArray());
-        return hub.invoke("Water",water.getDescriptorForType().getFullName(), version,ints);
+        return hub.invoke("Water",toPascalCase(water.getDescriptorForType().getFullName()), version,ints);
     }
 
     public <T extends com.google.protobuf.GeneratedMessage> void water (T water,Version version) throws ExecutionException, InterruptedException {
@@ -110,38 +126,54 @@ public class OtherTreeClient implements AutoCloseable {
 
 
 
-    public <T extends com.google.protobuf.GeneratedMessage> SignalRFuture<ChargeReference> futureCharge(T charge, Version version){
-        int[] ints= toIntArray(charge.toByteArray());
-        return hub.invoke(ChargeReference.class, "Charge", charge.getDescriptorForType().getFullName(), version, ints);
-    }
-    public <T extends com.google.protobuf.GeneratedMessage> SignalRFuture<ChargeReference> futureCharge(T charge){
-        return futureCharge(charge, new Version());
-    }
+            public <T extends com.google.protobuf.GeneratedMessage> SignalRFuture<ChargeReference> futureCharge(T charge, Version version){
+                int[] ints= toIntArray(charge.toByteArray());
+                return hub.invoke(ChargeReference.class, "Charge", toPascalCase(charge.getDescriptorForType().getFullName()), version, ints);
+            }
+            public <T extends com.google.protobuf.GeneratedMessage> SignalRFuture<ChargeReference> futureCharge(T charge){
+                return futureCharge(charge, new Version());
+            }
 
-    public SignalRFuture<Void> futureDischarge(ChargeReference reference){
-        return hub.invoke("Discharge", reference);
-    }
-
-
-    public <T extends com.google.protobuf.GeneratedMessage> ChargeReference charge (T charge,Version version) throws ExecutionException, InterruptedException {
-        return futureCharge(charge,version).get();
-    }
-    public <T extends com.google.protobuf.GeneratedMessage> ChargeReference charge (T charge) throws ExecutionException, InterruptedException {
-        return charge(charge, new Version());
-    }
-
-    public  void discharge(ChargeReference reference) throws ExecutionException, InterruptedException {
-        futureDischarge(reference).get();
-    }
+            public SignalRFuture<Void> futureDischarge(ChargeReference reference){
+                return hub.invoke("Discharge", reference);
+            }
 
 
-    public <T extends GeneratedMessage> void addOnThudListener(ThudListener<T> listener,Version version){
-        AnnotatedOnThudListener<T> handler = new AnnotatedOnThudListener<>(listener, version);
-        onThudListeners.add(handler);
-    }
+        public <T extends com.google.protobuf.GeneratedMessage> ChargeReference charge (T charge,Version version) throws ExecutionException, InterruptedException {
+            return futureCharge(charge,version).get();
+        }
+        public <T extends com.google.protobuf.GeneratedMessage> ChargeReference charge (T charge) throws ExecutionException, InterruptedException {
+            return charge(charge, new Version());
+        }
 
-    public <T extends GeneratedMessage> void addOnThudListener(ThudListener<T> listener){
-        addOnThudListener(listener, new Version());
+        public  void discharge(ChargeReference reference) throws ExecutionException, InterruptedException {
+            futureDischarge(reference).get();
+        }
+
+
+        public <T extends GeneratedMessage> void addOnThudListener(Class<T> expectedType,ThudListener<T> listener,Version version){
+            AnnotatedOnThudListener<T> handler = new AnnotatedOnThudListener<>(expectedType,listener, version);
+            onThudListeners.add(handler);
+        }
+
+        public <T extends GeneratedMessage> void addOnThudListener(Class<T> expectedType,ThudListener<T> listener){
+            addOnThudListener(expectedType, listener, new Version());
+        }
+
+        static String toPascalCase(String camelCase){
+            StringBuilder builder=new StringBuilder();
+            String[] strings=camelCase.split("\\.");
+            for(int i=0; i<strings.length; ++i){
+
+            if(strings[i].length()>0){
+                builder.append(Character.toUpperCase(strings[i].charAt(0)));
+                builder.append(strings[i].substring(1));
+                if(i!=strings.length-1){
+                    builder.append('.');
+                }
+            }
+        }
+        return builder.toString();
     }
 
     public <T extends GeneratedMessage>void removeOnThudListener(ThudListener<T> thudListener){
