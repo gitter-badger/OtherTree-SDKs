600e81ca7db4b71d0538a5e8d3db555610abb0a4
ncthbrt <nick@cuthbert.co.za>
2016-01-20 17:43:27 +0200

    Uploaded to Bintray

--- Java/android/src/wimt/othertree/client/android/OtherTreeAndroidClient.java
+++ Java/android/src/wimt/othertree/client/android/OtherTreeAndroidClient.java
@@ -1,7 +1,7 @@
 package wimt.othertree.client.android;
 import microsoft.aspnet.signalr.client.Platform;
-import wimt.othertree.client.*;
 import microsoft.aspnet.signalr.client.http.android.*;
+import wimt.othertree.client.OtherTreeClient;
 
 /**
  * Created by Nick Cuthbert on 05/01/2016.
@@ -15,6 +15,6 @@ public class OtherTreeAndroidClient extends OtherTreeClient{
         public OtherTreeAndroidClient(String url, String token, boolean useDefaultUrl){
             super();
             Platform.loadPlatformComponent(new AndroidPlatformComponent());
-            super.init(url, token,useDefaultUrl);
+            super.init(url, token, useDefaultUrl);
         }
 }
